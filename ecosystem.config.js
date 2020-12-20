8aeb8094fd7bf6a22907abd469bd2c6b66506a65
Luis Henrique Soler <blamoo@live.com>
2020-12-20 18:31:45 -0300

    wip

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Proxy Cameras',
+            script: 'empty',
+            interpreter: './camera-proxy',
+            env_production: {
+            }
+        },
+    ],
+};
